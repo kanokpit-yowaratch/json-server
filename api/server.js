6b145671a7e8e1d26e55173f4ad2bcfdebe5169a
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-01-30 13:46:07 +0700

    Simple API structure for Product Master

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+	console.log("App running on port: 3000");
+});
+
+module.exports = server;
