9e74e1a126d7da32e6e7f30aebaf5dbc7f8f8200
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-02-12 10:41:24 +0700

    Change path rewrite, install dotenv

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # json-server-product-master
 A simple API with json-server
+
+## Configuration
+`.env`
+
+```
+PORT=4004
+```
--- api/server.js
+++ api/server.js
@@ -2,12 +2,14 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("tmp/db.json");
 const middlewares = jsonServer.defaults();
+require('dotenv').config()
+const port = process.env.PORT || 4003;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-	console.log("App running on port: 3000");
+server.listen(port, () => {
+	console.log(`App running on port: ${port}`);
 });
 
 module.exports = server;
